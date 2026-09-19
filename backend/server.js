030249ebd6195be3910016b5aa70e4eaf3e58cf9
SOHAM BISHNU <sohambishnu2006@gmail.com>
2026-09-19 18:23:22 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
+const { initMeilisearch } = require('./src/config/meilisearch');
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  await connectDB();
+  await initMeilisearch();
+
+  app.listen(PORT, () => {
+    console.log(`[Server] Running on port ${PORT} (${process.env.NODE_ENV || 'development'})`);
+  });
+};
+
+start();
+
+process.on('unhandledRejection', (err) => {
+  console.error(`[UnhandledRejection] ${err.message}`);
+});
+
